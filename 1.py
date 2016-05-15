c4135a666bbfcb09f807e9ce0fedd6f73518ceac
davaiz <davaizilg@gmail.com>
2016-05-16 03:30:52 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,6 +6,13 @@ class Atbash:
         uppercase_code = {self.alphabet[i].upper():self.alphabet[-i-1].upper() for i in range(len(self.alphabet))}
         self._encode = dict(lowercase_code)
         self._encode.update(uppercase_code)
+        self._decode = dict(lowercase_code)
+        self._decode.update(uppercase_code)
+    def decode(self, line):
+        if len(line) == 1:
+            return self._decode[line] if line in self._decode else line
+        else:
+            return ''.join([self.decode(char) for char in line])
 
     def encode(self, line):
         if len(line) == 1:
